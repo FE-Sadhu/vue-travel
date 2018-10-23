c4ce71fa0cf4672766e570bd8d5c32e23c91a2d5
Sadhu <1714222385@qq.com>
2018-10-23 17:03:38 +0800

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,15 @@
 <template>
-
-<div id="app">
-  <router-view/>
-</div>
-
+    <div>
+      hello world
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
 <style>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }]
 })
