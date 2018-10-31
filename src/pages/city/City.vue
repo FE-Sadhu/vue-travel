013cec54dc56ebebb7d64feccaecda41e913b8c4
Sadhu <1714222385@qq.com>
2018-11-01 00:34:56 +0800

    city-search-logic finish

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list
       :hot="hotCities"
       :cities="cities"
--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,12 +1,82 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    <div>
+      <div class="search">
+          <input
+            class="search-input"
+            type="text"
+            placeholder="输入城市名或拼音"
+            v-model="keyword"
+          >
+      </div>
+      <div
+        class="search-content"
+        ref="search"
+        v-show="keyword"
+      >
+        <ul>
+          <li
+            v-for="item of list"
+            :key="item.id"
+            class="search-item border-bottom"
+            v-show="item"
+          >{{item.name}}</li>
+          <li
+           class="search-item border-bottom"
+           v-show="hasNoData"
+          >
+            没有找到匹配数据
+          </li>
+        </ul>
+      </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
+
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 ||
+                value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 1)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,4 +95,18 @@ export default {
       text-align center
       border-radius .06rem
       color #666   //input框中输入文字的颜色
+  .search-content
+    overflow hidden
+    position absolute
+    top 1.58rem
+    bottom 0
+    right 0
+    left 0
+    background #eee
+    z-index 1
+    .search-item
+      height .62rem
+      line-height .62rem
+      padding-left .2rem
+      color #666
 </style>
